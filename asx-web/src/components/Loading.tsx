22b829e0ff6521688366cb8466e0fc3bdde97512
Kamil Jakrzewski <kamiljakrzewski@tradeview.com.au>
2020-10-10 20:38:01 +1100

    use inline svg

--- asx-web/src/components/Loading.tsx
+++ asx-web/src/components/Loading.tsx
@@ -3,8 +3,17 @@ import React, { ReactElement } from 'react';
 function Loading(): ReactElement {
   return (
     <div className="asx-loading">
-      <img src="/loading.svg" alt="Loading, Please Wait" className="asx-loading-image" />
-    </div>
+      <svg width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+        <circle cx="50" cy="50" r="0" fill="none" stroke="#e90c0c" strokeWidth="2">
+          <animate attributeName="r" repeatCount="indefinite" dur="1.5s" values="0;46" keyTimes="0;1" keySplines="0 0.2 0.8 1" calcMode="spline" begin="-0.5s"></animate>
+          <animate attributeName="opacity" repeatCount="indefinite" dur="1.5s" values="1;0" keyTimes="0;1" keySplines="0.2 0 0.8 1" calcMode="spline" begin="-0.5s"></animate>
+        </circle>
+        <circle cx="50" cy="50" r="0" fill="none" stroke="#14bbce" strokeWidth="2">
+          <animate attributeName="r" repeatCount="indefinite" dur="1.5s" values="0;46" keyTimes="0;1" keySplines="0 0.2 0.8 1" calcMode="spline"></animate>
+          <animate attributeName="opacity" repeatCount="indefinite" dur="1.5s" values="1;0" keyTimes="0;1" keySplines="0.2 0 0.8 1" calcMode="spline"></animate>
+        </circle>
+      </svg>
+    </div >
   );
 }
 
